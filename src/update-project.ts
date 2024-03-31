515a95a15a6d1da84484410f5be5784e3f81b24a
Thomas Gish <tgish@thomasgish.us>
2024-03-31 14:02:44 -0700

    Fix syntax

--- src/update-project.ts
+++ src/update-project.ts
@@ -35,7 +35,7 @@ export async function fetchContentMetadata(
         }
       }
       field: fieldValueByName(name: $fieldName) {
-	... on ProjectV2ItemFieldIterationValue) {
+	... on ProjectV2ItemFieldIterationValue {
 	  value: title
 	}
         ... on ProjectV2ItemFieldSingleSelectValue {
